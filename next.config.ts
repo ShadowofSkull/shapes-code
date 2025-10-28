83eed86cc5ee6d44c0927fc2573c84431ab470e8
ShadowofSkull <zhplana@gmail.com>
2025-10-28 19:48:29 +0800

    database_url not found fix

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,9 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+    env: {
+    DATABASE_URL: process.env.DATABASE_URL ?? "",
+  }, // to fix prisma not reading env variable during deployment
 };
 
 export default nextConfig;
